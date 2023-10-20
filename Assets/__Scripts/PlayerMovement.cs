38b647b0f6a094f061446fe813fbd28e897f134b
Paweł Kupczak <pawel.kupczak.sc@gmail.com>
2023-10-21 00:38:30 +0200

    Rewrite movement script, now allows for a simple movement and jumping

--- Assets/__Scripts/PlayerMovement.cs
+++ Assets/__Scripts/PlayerMovement.cs
@@ -2,63 +2,85 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float playerSpeed = 5f;
-    [SerializeField] float jumpHeight = 1.5f;
-    [SerializeField] float jumpCooldown = 0.5f; // Add a jump cooldown
-    [SerializeField] float groundRaycastDistance = 0.2f; // Distance for ground raycast
+    [Header("Parameters")]
+    [SerializeField] private float movementSpeed = 5.0f;
+    [SerializeField] private float jumpHeight = 1.5f;
+    [SerializeField] private float gravityForce = -9.81f;
+    [SerializeField] private float jumpCooldown = 0.2f;
+    [SerializeField] private KeyCode jumpKey = KeyCode.Space;
 
-    private Vector3 playerVelocity;
-    private bool groundedPlayer;
+    [Tooltip("Distance for which ground layer is checked for")]
+    [SerializeField] private float groundCastCheckDistance = 0.1f;
+
+    [Tooltip("Distance for which ground (ceiling) layer is checked for")]
+    [SerializeField] private float ceilingCastCheckDistance = 0.1f;
+
+    [Space]
+    [Header("Masks")]
+    [SerializeField] private LayerMask groundMask;
+
+    [Space]
+    [Header("Visible internal variables (do not change)")]
+    [SerializeField] private Vector3 playerVelocity;
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private bool hitCeiling;
+
     private CharacterController controller;
-    private Vector3 forward, right;
-    private float gravityValue = -9.81f;
-    private float lastJumpTime;
+    private Vector3 forward;
+    private Vector3 right;
+    private float lastJumpTimestamp;
 
     private void Awake()
     {
         controller = GetComponent<CharacterController>();
         forward = Camera.main.transform.forward;
-        forward.y = 0;
+        forward.y = 0.0f;
         forward = Vector3.Normalize(forward);
-        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+        right = Quaternion.Euler(Vector3.up * 90.0f) * forward;
     }
 
-    void Update()
+    private void Update()
     {
-        groundedPlayer = IsGrounded();
+        isGrounded = CheckForGroundCollision();
+        hitCeiling = CheckForCeilingCollision();
 
-        if (groundedPlayer && playerVelocity.y < 0)
+        Vector3 forwardMovement = forward * Input.GetAxisRaw("Vertical");
+        Vector3 rightMovement = right * Input.GetAxisRaw("Horizontal");
+        Vector3 wishDir = Vector3.Normalize(rightMovement + forwardMovement) * movementSpeed;
+
+        if (isGrounded && playerVelocity.y < 0.0f || hitCeiling && playerVelocity.y > 0.0f)
         {
-            playerVelocity.y = 0f;
+            playerVelocity.y = 0.0f;
         }
 
-        Movement();
-
-        if (Input.GetKeyDown(KeyCode.Space) && groundedPlayer && Time.time - lastJumpTime >= jumpCooldown)
+        if (Input.GetKey(jumpKey) && isGrounded && Time.time - lastJumpTimestamp >= jumpCooldown)
         {
-            lastJumpTime = Time.time;
-            playerVelocity.y = Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+            lastJumpTimestamp = Time.time;
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -3.0f * gravityForce);
         }
 
-        playerVelocity.y += gravityValue * Time.deltaTime;
+        playerVelocity.x = wishDir.x;
+        playerVelocity.y += gravityForce * Time.deltaTime;
+        playerVelocity.z = wishDir.z;
+
         controller.Move(playerVelocity * Time.deltaTime);
     }
 
-    private void Movement()
+    private bool CheckForGroundCollision()
     {
-        Vector3 rightMovement = right * Input.GetAxis("Horizontal");
-        Vector3 upMovement = forward * Input.GetAxis("Vertical");
-
-        Vector3 moveVector = rightMovement + upMovement;
-        float magnitude = Mathf.Clamp01(moveVector.magnitude) * playerSpeed;
-        moveVector.Normalize();
-
-        controller.SimpleMove(moveVector * magnitude);
+        return Physics.CheckSphere(transform.position + Vector3.down, groundCastCheckDistance, groundMask) ||
+            Physics.CheckSphere(transform.position + Vector3.down + Vector3.right, groundCastCheckDistance, groundMask) ||
+            Physics.CheckSphere(transform.position + Vector3.down + Vector3.left, groundCastCheckDistance, groundMask) ||
+            Physics.CheckSphere(transform.position + Vector3.down + Vector3.back, groundCastCheckDistance, groundMask) ||
+            Physics.CheckSphere(transform.position + Vector3.down + Vector3.forward, groundCastCheckDistance, groundMask);
     }
 
-    private bool IsGrounded()
-    {      
-        Ray groundRay = new Ray(transform.position, Vector3.down);  
-        return Physics.Raycast(groundRay, groundRaycastDistance);
+    private bool CheckForCeilingCollision()
+    {
+        return Physics.CheckSphere(transform.position + Vector3.up, ceilingCastCheckDistance, groundMask) ||
+            Physics.CheckSphere(transform.position + Vector3.up + Vector3.right, ceilingCastCheckDistance, groundMask) ||
+            Physics.CheckSphere(transform.position + Vector3.up + Vector3.left, ceilingCastCheckDistance, groundMask) ||
+            Physics.CheckSphere(transform.position + Vector3.up + Vector3.back, ceilingCastCheckDistance, groundMask) ||
+            Physics.CheckSphere(transform.position + Vector3.up + Vector3.forward, ceilingCastCheckDistance, groundMask);
     }
 }
