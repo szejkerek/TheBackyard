7de7e955b0b7ad37aa69306058ac68994d5138bf
Szejkerek <bartekk.gordon@gmail.com>
2023-10-20 20:35:59 +0200

    Remove assemblies

---
+++ Assets/__Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private PlayerControls input = null;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+}
