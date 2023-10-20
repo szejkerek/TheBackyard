07d47c2abdcf1574de6857ec1ce42f25b9e3e28f
Szejkerek <bartekk.gordon@gmail.com>
2023-10-20 23:21:04 +0200

    Addressables package + persistent system

---
+++ Assets/__Scripts/PersistentSystems/SystemsBootstrapper.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public static class SystemsBootstrapper
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    public static void Execute()
+    {
+        Object.DontDestroyOnLoad(Addressables.InstantiateAsync("PersistentSystems").WaitForCompletion());
+    }
+}
