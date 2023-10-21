c415a2da84cfaeb4e8f9a1014d52c22630688157
Paweł Kupczak <pawel.kupczak.sc@gmail.com>
2023-10-21 15:12:57 +0200

    Implement basic interface

--- Assets/__Scripts/Utility/Clock.cs
+++ Assets/__Scripts/Utility/Clock.cs
@@ -4,11 +4,42 @@ using UnityEngine;
 
 public class Clock
 {
-    private float startTimestamp;
-    private bool isActive;
+    private float startTimestamp = 0.0f;
+    private float accumulatedTime = 0.0f;
+    private bool isActive = false;
+
+    public bool IsRunning => isActive;
 
     public Clock()
     {
         startTimestamp = Time.time;
+    }
+
+    public void Start()
+    {
+        isActive = true;
+        startTimestamp = Time.time;
+    }
+
+    public void Stop()
+    {
+        isActive = false;
+        accumulatedTime += Time.time - startTimestamp;
+    }
+
+    public void Restart()
+    {
+        accumulatedTime = 0.0f;
+        Start();
+    }
+
+    float GetElapsedTimeInSeconds()
+    {
+        return accumulatedTime + (Time.time - startTimestamp);
+    }
+
+    float GetElapsedTimeInMiliseconds()
+    {
+        return GetElapsedTimeInSeconds() * 1000.0f;
     }
 }
