6365172b82cc40e5638ec45c314d4282c17df0e2
Paweł Kupczak <pawel.kupczak.sc@gmail.com>
2023-10-21 03:42:31 +0200

    Fix: don't do anything if PlayerMovement doesn't exist

--- Assets/__Scripts/CameraTargetFollower.cs
+++ Assets/__Scripts/CameraTargetFollower.cs
@@ -2,19 +2,34 @@ using UnityEngine;
 
 public class CameraTargetFollower : MonoBehaviour
 {
-    [SerializeField] private PlayerMovement playerMovement;
     [SerializeField] private float speed;
+    private PlayerMovement playerMovement;
 
     public Vector3 initOffset;
 
+    private bool validTarget = true;
+
+    private void Awake()
+    {
+        playerMovement = FindObjectOfType<PlayerMovement>();
+
+        if(playerMovement == null)
+        {
+            validTarget = false;
+        }
+    }
     void Start()
     {
+        if (!validTarget) return;
+
         initOffset = transform.position;
         transform.position = playerMovement.transform.position + initOffset + Vector3.up * playerMovement.JumpHeight;
     }
 
     void LateUpdate()
     {
+        if (!validTarget) return;
+
         transform.position = Vector3.Lerp(transform.position, playerMovement.transform.position + initOffset + Vector3.up * playerMovement.JumpHeight, speed * Time.deltaTime);
     }
 }
