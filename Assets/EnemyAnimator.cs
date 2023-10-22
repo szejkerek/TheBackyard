4a925a9c7bff88c9b7534393aac67ce8676acbfd
Biurito <oto.wojtek@gmail.com>
2023-10-22 15:34:10 +0200

    enemy animator script

---
+++ Assets/EnemyAnimator.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAnimator : MonoBehaviour
+{
+    private Transform sprite;
+    private Animator animator;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        sprite = transform.Find("Sprite").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Mathf.Abs(agent.velocity.x) > 0.05f)
+        {
+            sprite.localScale = new Vector3(Mathf.Sign(Input.GetAxisRaw("Horizontal")) * -1, 1, 1);
+        }
+        if (Mathf.Abs(agent.velocity.x) > 0.05f || (Mathf.Abs(agent.velocity.y) > 0.05f)) animator.SetBool("isIdle", false);
+        else animator.SetBool("isIdle", true);
+    }
+}
