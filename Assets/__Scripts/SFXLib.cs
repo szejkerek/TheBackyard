e9a0288bad88038dca620548c49b966150104d5f
Szejkerek <bartekk.gordon@gmail.com>
2023-10-22 14:50:20 +0200

    Add audio manager

---
+++ Assets/__Scripts/SFXLib.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "AudioData/SFXLib", fileName = "SFXLib")]
+public class SFXLib : ScriptableObject
+{
+    [field: SerializeField] public AudioClip Test { private set; get; }
+}
