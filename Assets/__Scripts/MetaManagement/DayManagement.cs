5a95ad343f8e43d48838a594ac4efce852bfe25d
Szejkerek <bartekk.gordon@gmail.com>
2023-10-22 12:34:45 +0200

    Minigame picker

--- Assets/__Scripts/MetaManagement/DayManagement.cs
+++ Assets/__Scripts/MetaManagement/DayManagement.cs
@@ -1,43 +1,45 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 public class DayManagement : Singleton<DayManagement>
-{ 
+{
     [SerializeField] TMP_Text timeText;
-    [SerializeField] TMP_InputField possibleWin;
-    [SerializeField] TMP_InputField possibleLoss;
-    [SerializeField] TMP_InputField timeLoss;
+    [SerializeField] GameObject minigamePicker;
+    [SerializeField] GameObject characterPicker;
+
 
     CycleManager cycleManager;
-
+    ArenaInformation arenaInformation;
     private void Start()
     {
+        characterPicker.gameObject.SetActive(false);
         cycleManager = MetaGameplayManager.Instance.CycleManager;
         timeText.text = $"Time left: {cycleManager.HoursLeft}h";
     }
 
     public void GoIntoArena(int characterIndex) 
     {
+        if (arenaInformation == null)
+        {
+            Debug.LogWarning($"Arena information is null");
+            return;
+        }
+        arenaInformation.character = GameManager.Instance.PlayableCharacters[characterIndex];
+        GameManager.Instance.SetArenaInformation(arenaInformation);
+        SceneManager.Instance.LoadScene(2);
+    }
+
+    public void SetArenaInformation(ArenaInformation newArenaInformation)
+    {
         int hoursLeft = MetaGameplayManager.Instance.CycleManager.HoursLeft;
-        ArenaInformation info = GatherArenaInformatio();
-        if(info.timeLoss >= hoursLeft)
+        arenaInformation = newArenaInformation;
+        if (arenaInformation.timeLoss >= hoursLeft)
         {
             Debug.LogWarning($"Cannot Go to this arena with {hoursLeft} hours left");
             return;
         }
-        info.character = GameManager.Instance.PlayableCharacters[characterIndex];
-        GameManager.Instance.SetArenaInformation(info);
-        SceneManager.Instance.LoadScene(2);
-    }
-
-    private ArenaInformation GatherArenaInformatio()
-    {
-        ArenaInformation temp = new ArenaInformation();
-        temp.character = null;
-        temp.moneyWin = int.Parse(possibleWin.text);
-        temp.moneyLoss = int.Parse(possibleLoss.text);
-        temp.timeLoss = int.Parse(timeLoss.text);
-
-        return temp;
+        minigamePicker.gameObject.SetActive(false);
+        characterPicker.gameObject.SetActive(true);
     }
 }
