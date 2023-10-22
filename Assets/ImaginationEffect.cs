5a37fd3ffb2a92d6513c2c381a1fc65f74e69907
Biurito <oto.wojtek@gmail.com>
2023-10-22 16:28:12 +0200

    Imagination fx

--- Assets/ImaginationEffect.cs
+++ Assets/ImaginationEffect.cs
@@ -4,5 +4,24 @@ using UnityEngine;
 
 public class ImaginationEffect : MonoBehaviour
 {
-    
+    private ProjectImagination[] pi;
+    private void Start()
+    {
+        pi = GameObject.FindObjectsOfType<ProjectImagination>();
+    }
+
+    private void Update()
+    {
+     foreach(ProjectImagination pim in pi)
+        {
+            if(Vector3.Distance(pim.transform.position, transform.position) < 2f)
+            {
+                pim.ChangeState(true);
+            }
+            else
+            {
+                pim.ChangeState(false);
+            }
+        }
+    }
 }
