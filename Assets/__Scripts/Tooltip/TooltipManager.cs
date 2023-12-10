de4a458ea16834e0c965d28b2ce6c2802bc9cea5
megabombus-rex <czechumennakompie@gmail.com>
2023-12-10 18:13:38 +0100

    Add comments for tooltip

--- Assets/__Scripts/Tooltip/TooltipManager.cs
+++ Assets/__Scripts/Tooltip/TooltipManager.cs
@@ -2,18 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Manages the display and hiding of tooltips in the game.
+/// </summary>
 public class TooltipManager : Singleton<TooltipManager>
 {
+    /// <summary>
+    /// Gets or sets the Tooltip component used for displaying tooltips.
+    /// </summary>
     public Tooltip tooltip;
+
+    /// <summary>
+    /// Gets or sets the delay before showing the tooltip.
+    /// </summary>
     public float delay = 0.5f;
 
     private Coroutine showCoroutine;
+
+    /// <summary>
+    /// Shows a tooltip with the specified content and header text.
+    /// </summary>
+    /// <param name="content">The content text of the tooltip.</param>
+    /// <param name="header">The header text of the tooltip (optional).</param>
     public void Show(string content, string header = "")
     {
         showCoroutine = StartCoroutine(ShowWithDelay(content, header));
     }
 
-    public void Hide() 
+    /// <summary>
+    /// Hides the currently displayed tooltip.
+    /// </summary>
+    public void Hide()
     {
         if (showCoroutine != null)
         {
@@ -22,6 +41,12 @@ public class TooltipManager : Singleton<TooltipManager>
         tooltip.gameObject.SetActive(false);
     }
 
+    /// <summary>
+    /// Coroutine to show the tooltip with a delay.
+    /// </summary>
+    /// <param name="content">The content text of the tooltip.</param>
+    /// <param name="header">The header text of the tooltip (optional).</param>
+    /// <returns>Yield instruction for waiting.</returns>
     private IEnumerator ShowWithDelay(string content, string header)
     {
         yield return new WaitForSeconds(delay);
--- Assets/__Scripts/Tooltip/TooltipTrigger.cs
+++ Assets/__Scripts/Tooltip/TooltipTrigger.cs
@@ -3,16 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+/// <summary>
+/// Triggers the display of a tooltip when the mouse pointer enters the UI element
+/// and hides the tooltip when the mouse pointer exits.
+/// </summary>
 public class TooltipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    /// <summary>
+    /// Gets or sets the header text for the tooltip.
+    /// </summary>
     [SerializeField] private string header;
+
+    /// <summary>
+    /// Gets or sets the content text for the tooltip.
+    /// </summary>
     [SerializeField, TextArea(3, 1)] private string content;
 
+    /// <summary>
+    /// Called when the mouse pointer enters the UI element.
+    /// Displays the tooltip with the specified content and header.
+    /// </summary>
+    /// <param name="eventData">The pointer event data.</param>
     public void OnPointerEnter(PointerEventData eventData)
     {
         TooltipManager.Instance.Show(content, header);
     }
 
+    /// <summary>
+    /// Called when the mouse pointer exits the UI element.
+    /// Hides the displayed tooltip.
+    /// </summary>
+    /// <param name="eventData">The pointer event data.</param>
     public void OnPointerExit(PointerEventData eventData)
     {
         TooltipManager.Instance.Hide();
